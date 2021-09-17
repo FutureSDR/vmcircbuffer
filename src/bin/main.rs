1bd78308deab781e2cde59afa38a97ac18e946ce
Bastian Bloessl <mail@bastibl.net>
2021-09-17 11:06:59 +0200

    dsp

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 use std::iter::repeat_with;
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::{Arc, Barrier};
 use std::thread;
 use std::thread::JoinHandle;
 
@@ -8,6 +8,7 @@ use double_mapped_circular_buffer::CircularReader;
 
 // struct Kernel<A, B> {}
 
+#[allow(clippy::type_complexity)]
 struct Source<A: Send + Sync + 'static> {
     f: Option<Box<dyn FnMut(&mut [A]) -> Option<usize> + Send + Sync + 'static>>,
 }
@@ -21,18 +22,14 @@ impl<A: Send + Sync> Source<A> {
 
     pub fn run(
         &mut self,
-        sync: Arc<(Mutex<bool>, Condvar)>,
+        barrier: Arc<Barrier>,
     ) -> (CircularReader<A>, JoinHandle<()>) {
         let w = Circular::new::<A>().unwrap();
         let r = w.add_reader();
         let mut f = self.f.take().unwrap();
 
         let handle = thread::spawn(move || {
-            let (lock, cvar) = &*sync;
-            let mut started = lock.lock().unwrap();
-            while !*started {
-                started = cvar.wait(started).unwrap();
-            }
+            barrier.wait();
 
             loop {
                 let s = w.slice();
@@ -55,11 +52,7 @@ struct Sink<A: Clone + Send + Sync + 'static> {
 }
 
 impl<A: Clone + Send + Sync + 'static> Sink<A> {
-    pub fn new() -> Sink<A> {
-        Self::with_capacity(0)
-    }
-
-    pub fn with_capacity(capacity: usize) -> Sink<A> {
+    pub fn new(capacity: usize) -> Sink<A> {
         Sink {
             items: Some(Vec::with_capacity(capacity)),
         }
@@ -68,59 +61,49 @@ impl<A: Clone + Send + Sync + 'static> Sink<A> {
     pub fn run(
         &mut self,
         r: CircularReader<A>,
-        sync: Arc<(Mutex<bool>, Condvar)>,
+        barrier: Arc<Barrier>,
     ) -> JoinHandle<Vec<A>> {
         let mut items = self.items.take().unwrap();
 
         thread::spawn(move || {
-            let (lock, cvar) = &*sync;
-            let mut started = lock.lock().unwrap();
-            while !*started {
-                started = cvar.wait(started).unwrap();
-            }
+            barrier.wait();
 
             while let Some(s) = r.slice() {
                 items.extend_from_slice(s);
                 r.consume(s.len());
             }
 
-            println!("source terminated");
+            println!("sink terminated");
             items
         })
     }
 }
 
 fn main() {
-    let w = Circular::new::<f32>().unwrap();
-    let r = w.add_reader();
-
-    let input: Vec<f32> = repeat_with(rand::random::<f32>).take(1231233).collect();
-    let mut output = vec![0f32; input.len()];
+    let n_samples = 1231233;
 
     let mut i = 0;
-    let t1 = thread::spawn(move || {
-        while i < input.len() {
-            let s = w.slice();
-            let len = std::cmp::min(s.len(), input.len() - i);
+    let input: Vec<f32> = repeat_with(rand::random::<f32>).take(n_samples).collect();
+
+    let mut src = Source::new(move |s: &mut [f32]| -> Option<usize> {
+        if i < n_samples {
+            let len = std::cmp::min(s.len(), n_samples - i);
             s[0..len].clone_from_slice(&input[i..i + len]);
             i += len;
-            w.produce(len);
+            Some(len)
+        } else {
+            None
         }
-
-        println!("hello world one");
     });
 
-    let mut i = 0;
-    let t2 = thread::spawn(move || {
-        while let Some(s) = r.slice() {
-            output[i..i + s.len()].copy_from_slice(s);
-            r.consume(s.len());
-            i += s.len();
-        }
+    let mut snk = Sink::new(n_samples);
 
-        println!("received {} items", i);
-    });
+    let barrier = Arc::new(Barrier::new(3));
+    let (reader, _) = src.run(Arc::clone(&barrier));
+    let handle = snk.run(reader, Arc::clone(&barrier));
 
-    t1.join().unwrap();
-    t2.join().unwrap();
+    barrier.wait();
+    println!("started all");
+    let output = handle.join().unwrap();
+    println!("rxed vec of len {}", output.len());
 }
